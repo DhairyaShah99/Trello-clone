3a1d30b124bbd99b43c6460846dcc735764ed756
DhairyaShah99 <shahdhairya2607@gmail.com>
2023-07-07 17:25:38 +0530

    Second-Commit-dnd

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,26 +1,47 @@
 'use client'
 
-import { DragDropContext, Droppable } from 'react-beautiful-dnd';
+import { DragDropContext, DropResult, Droppable } from 'react-beautiful-dnd';
 import React, { useEffect } from 'react'
 import { useBoardStore } from '@/store/BoardStore';
+import Column from './Column';
 
 function Board() {
-    const getBoard = useBoardStore((state) => state.getBoard);
+    const[board, getBoard]  = useBoardStore((state) => [
+      state.board, 
+      state.getBoard]);
+
     useEffect(() => {
         getBoard();
     },[getBoard]);
+    //console.log(board);
 
+    const handleDragEnd = (result: DropResult) => {
+
+    }
   return (
-    <h1>Hellloooo</h1>
-    // <DragDropContext>
-    //     <Droppable droppableId='board' direction='horizontal' type='column'>
-    //         {(provided) => (
-    //             <div>
-    //                 {/* rendering all the main boards/colums */}
-    //             </div>
-    //         )}
-    //     </Droppable>
-    // </DragDropContext>
+    //<h1>Hellloooo</h1>
+    <DragDropContext onDragEnd={handleDragEnd}>
+        <Droppable droppableId='board' direction='horizontal' type='column'>
+            {(provided) => (
+                <div
+                  className="grid grid-cols-1 md:grid-cols-3 gap-5 max-w-7xl mx-auto"
+                  {...provided.droppableProps}
+                  ref={provided.innerRef}
+                >
+                    {
+                       Array.from(board.columns.entries()).map(([id, column], index) => (
+                        <Column
+                          key={id}
+                          id={id}
+                          todos={column.todos}
+                          index={index}
+                        />
+                      ))
+                    }
+                </div>
+            )}
+        </Droppable>
+    </DragDropContext>
   );
 };
 
